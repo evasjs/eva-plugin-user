96283f41d1fa3517bc95a63f55346b5edf1afea2
tobewhatwewant <tobewhatwewant@gmail.com>
2017-07-09 14:17:43 +0800

    :bug:..

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
  * @Author: eason
  * @Date:   2017-07-09T13:51:37+08:00
  * @Last modified by:   eason
- * @Last modified time: 2017-07-09T13:51:55+08:00
+ * @Last modified time: 2017-07-09T14:17:27+08:00
  */
-module.exports = require('user');
+module.exports = require('./user');
